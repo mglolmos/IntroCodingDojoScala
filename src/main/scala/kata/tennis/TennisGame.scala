a6d10dd6a5ede135228c6405673cefad94a66634
Miguel Olmos <mgl.olmos@gmail.com>
2015-11-15 19:15:42 +0100

    Finished coding dojo

--- src/main/scala/kata/tennis/TennisGame.scala
+++ src/main/scala/kata/tennis/TennisGame.scala
@@ -2,12 +2,61 @@ package kata.tennis
 
 sealed trait Player
 
-sealed trait Score
+sealed trait Score {
+  def up() : Score
+}
 
-case class TennisGame(player1: Score, player2: Score) {
+case class TennisGame(gameScore: GameScore) {
 
-  def play(winner: Player): TennisGame = ???
+  def play(winner: Player): TennisGame = {
+    val game = (gameScore.service, gameScore.receiver, winner) match {
+      case (Forty, Forty, Player1) => TennisGame(GameScore(Advantage, Forty))
+      case (Forty, Forty, Player2) => TennisGame(GameScore(Forty, Advantage))
+      case (_, Advantage, Player1) => TennisGame(GameScore(Forty, Forty))
+      case (Advantage, _, Player2) => TennisGame(GameScore(Forty, Forty))
+      case (_, _, Player1) => TennisGame(gameScore.winService())
+      case (_, _, Player2) => TennisGame(gameScore.winReceiver())
+    }
 
+    game
+  }
+}
+
+case class GameScore(service : Score, receiver : Score) {
+  def winService() : GameScore = {
+    GameScore(service.up(), receiver)
+  }
+
+  def winReceiver() : GameScore = {
+    GameScore(service, receiver.up())
+  }
 }
 
 
+case object Love extends Score {
+  val up : Score = { Fifteen }
+}
+
+case object Fifteen extends Score {
+  val up : Score = { Thirty }
+}
+
+case object Thirty extends Score {
+  val up : Score = { Forty }
+}
+
+case object Forty extends Score {
+  val up : Score = { Win }
+}
+
+case object Advantage extends Score {
+  val up : Score = { Win }
+}
+
+case object Win extends Score {
+  val up : Score = { Win }
+}
+
+case object Player1 extends Player
+
+case object Player2 extends Player
--- src/test/scala/kata/tennis/TennisGameSpec.scala
+++ src/test/scala/kata/tennis/TennisGameSpec.scala
@@ -7,8 +7,59 @@ class TennisGameSpec extends FlatSpec with ShouldMatchers {
 
   behavior of "Tennis Game"
 
-  it should "return 15-0" in {
-    true should equal(true)
+  it should "if 0-0 and win player1 return 15-0" in {
+    TennisGame(GameScore(Love, Love)).play(Player1) should equal(TennisGame(GameScore(Fifteen, Love)))
   }
 
+  it should "if 0-0 and win player2 return 0-15" in {
+    TennisGame(GameScore(Love, Love)).play(Player2) should equal(TennisGame(GameScore(Love, Fifteen)))
+  }
+
+  it should "if 15-0 and win player1 return 30-0" in {
+    TennisGame(GameScore(Fifteen, Love)).play(Player1) should equal(TennisGame(GameScore(Thirty, Love)))
+  }
+
+  it should "if 0-15 and win player2 return 0-30" in {
+    TennisGame(GameScore(Love, Fifteen)).play(Player2) should equal(TennisGame(GameScore(Love, Thirty)))
+  }
+
+  it should "if 30-0 and win player1 return 40-0" in {
+    TennisGame(GameScore(Thirty, Love)).play(Player1) should equal(TennisGame(GameScore(Forty, Love)))
+  }
+
+  it should "if 0-30 and win player2 return 0-40" in {
+    TennisGame(GameScore(Love, Thirty)).play(Player2) should equal(TennisGame(GameScore(Love, Forty)))
+  }
+
+  it should "if 40-30 and win player1 return Win-30" in {
+    TennisGame(GameScore(Forty, Thirty)).play(Player1) should equal(TennisGame(GameScore(Win, Thirty)))
+  }
+
+  it should "if 30-40 and win player2 return 30-Win" in {
+    TennisGame(GameScore(Thirty, Forty)).play(Player2) should equal(TennisGame(GameScore(Thirty, Win)))
+  }
+
+  it should "if 40-40 and win player1 return Advantage-40" in {
+    TennisGame(GameScore(Forty, Forty)).play(Player1) should equal(TennisGame(GameScore(Advantage, Forty)))
+  }
+
+  it should "if 40-40 and win player2 return 40-Advantage" in {
+    TennisGame(GameScore(Forty, Forty)).play(Player2) should equal(TennisGame(GameScore(Forty, Advantage)))
+  }
+
+  it should "if 40-Advantage and win player1 return 40-40" in {
+    TennisGame(GameScore(Forty, Advantage)).play(Player1) should equal(TennisGame(GameScore(Forty, Forty)))
+  }
+
+  it should "if Advantage-40 and win player2 return 40-40" in {
+    TennisGame(GameScore(Advantage, Forty)).play(Player2) should equal(TennisGame(GameScore(Forty, Forty)))
+  }
+
+  it should "if Advantage-40 and win player1 return Win-40" in {
+    TennisGame(GameScore(Advantage, Forty)).play(Player1) should equal(TennisGame(GameScore(Win, Forty)))
+  }
+
+  it should "if 40-Advantage and win player2 return 40-Win" in {
+    TennisGame(GameScore(Forty, Advantage)).play(Player2) should equal(TennisGame(GameScore(Forty, Win)))
+  }
 }
